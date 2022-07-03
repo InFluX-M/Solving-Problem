7dd72de3116b5d8c709a1d51e8a19dce400f8e46
matin-azami <matin.azami.03@gmail.com>
2022-07-04 00:25:22 +0430

    1

---
+++ Advanced Algorithmic Thinking and Data Structures Quera/Chapter2/SearchInSequence1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+//جست‌وجو در دنباله ۱
+
+int main()
+{
+    int n, q;
+    cin >> n;
+    cin >> q;
+
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+    
+
+    for (int i = 1; i <= q; i++)
+    {
+        int x;
+        cin >> x;
+        int num = 0;
+        for (int j = 0; j < n; j++) if (a[j] < x) num++;
+
+        cout << num << "\n";
+    }
+}
---
+++ Advanced Algorithmic Thinking and Data Structures Quera/Chapter2/SearchInSequence2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+//جست‌وجو در دنباله 2
+
+int main()
+{
+    int n, q;
+    cin >> n;
+    cin >> q;
+
+    int* a = new int[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    int M = 0;
+    for (int i = 0; i < n; i++) if (a[i] > M) M = a[i];
+
+    int* c = new int[M];
+    for (int i = 0; i < M; i++) c[i] = 0;
+    
+    for (int i = 0; i < n; i++) c[a[i] - 1]++;
+    
+
+    int* p = new int[M];
+    for (int i = 0; i < M; i++) p[i] = 0;
+    
+    for (int i = 1; i < M; i++) p[i] = c[i - 1] + p[i - 1];
+
+    for (int i = 1; i <= q; i++)
+    {
+        int x;
+        cin >> x;
+
+        if (x > M) cout << n;
+        else cout << p[x - 1];
+
+        cout << "\n";
+    }
+
+
+}
