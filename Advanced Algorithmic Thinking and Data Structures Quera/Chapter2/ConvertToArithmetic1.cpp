e45fe287c79f7103a362cd9173c45fbc308d7b6a
matin-azami <matin.azami.03@gmail.com>
2022-07-05 21:44:01 +0430

    Ex

---
+++ Advanced Algorithmic Thinking and Data Structures Quera/Chapter2/ConvertToArithmetic1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+//تبدیل به حسابی ۱
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+
+	int* a = new int[n];
+	int* b = new int[n];
+
+	int m = 101;
+	int M = -101;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (m > a[i]) m = a[i];
+		if (M < a[i]) M = a[i];
+	}
+	
+	int nOMin = INT_MAX;
+	for (int i = m - (n - 1) * k; i <= M; i++)
+	{
+		int nO = 0;
+		for (int j = 0; j < n; j++)
+		{
+			nO += abs(a[j] - (i + (j * k)));
+		}
+
+		if (nOMin > nO) nOMin = nO;
+	}
+
+	cout << nOMin;
+
+	delete[] a;
+	delete[] b;
+
+}
--- Advanced Algorithmic Thinking and Data Structures Quera/Chapter2/SearchInSequence2.cpp
+++ Advanced Algorithmic Thinking and Data Structures Quera/Chapter2/SearchInSequence2.cpp
@@ -37,5 +37,4 @@ int main()
         cout << "\n";
     }
 
-
 }
