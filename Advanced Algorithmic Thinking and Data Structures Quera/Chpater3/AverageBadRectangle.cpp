b18642a0835f2276e82d406d4e06ed7bb2f82434
matin-azami <matin.azami.03@gmail.com>
2022-07-09 17:37:41 +0430

    Ex

---
+++ Advanced Algorithmic Thinking and Data Structures Quera/Chpater3/AverageBadRectangle.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+#define ll long long int
+
+//مستطیل بد متوسط
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    ll nO = 0;
+    for (ll i = 1; i <= floor(n / 3.0); i++)
+    {
+        int temp = (floor(n / 2.0) - (2 * i) + 1);
+        ll lower = max(0, temp);
+        ll upper = (floor((n - 3.0 * i) / 2.0));
+
+        nO += (upper - lower + 1);
+    }
+
+    cout << nO % (1000000000 + 7);
+}
